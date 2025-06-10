b0d0579a181053166fe15b77ed8b76220608de41
melinaasantillan <129870878+melinaasantillan@users.noreply.github.com>
2025-06-09 19:09:18 -0500

    feat: add Shared

---
+++ src/shared/components/create.component.vue
@@ -0,0 +1,44 @@
+<script>
+
+export default {
+  name: "create",
+  props: {
+    entity: null,
+    visible: Boolean(),
+    entityName: '',
+    size: 'default',
+  },
+  emits: ['cancel-action-requested', 'save-action-requested'],
+  data(){
+    return {
+      submitted: false
+    }
+  },
+  methods: {
+    onCancel() {
+      this.$emit('cancel-action-requested');
+    },
+    onSave() {
+      this.$emit('save-action-requested', this.entity)
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-dialog v-bind:visible="visible" :modal="true" class="p-fluid" header="Create" entity-name="Review">
+    <template #header>
+      Create a review
+    </template>
+    <slot name="content"></slot>
+    <template #footer>
+      <div class="flex justify-content-end">
+        <pv-button type="button" label="Create" class="p-button-text" icon="pi pi-check" @click="onSave"/>
+        <pv-button type="button" label="Cancel" severity="secondary" class="p-button-text" icon="pi pi-times" @click="onCancel"/>
+      </div>
+    </template>
+  </pv-dialog>
+</template>
+
+<style scoped>
+</style>
---
+++ src/shared/components/data-manager.component.vue
@@ -0,0 +1,37 @@
+<script>
+import {FilterMatchMode} from "@primevue/core";
+
+export default {
+  name: "data-manager",
+  inheritAttrs: false,
+  props: {
+    items: {type: Array, required: true},
+  },
+  emits: ['new-item-requested'],
+  data() {
+    return {
+      selectedItems: [],
+      filters: null
+    }
+  },
+  methods: {
+    initFilters() {
+      this.filters = {global: {value: null, matchMode: FilterMatchMode.CONTAINS}};
+    },
+    newItem() {
+      this.$emit('new-item-requested');
+    },
+  },
+  created() {
+    this.initFilters();
+  },
+}
+</script>
+
+<template>
+  <pv-button class="mr-2" icon="pi pi-plus" label="New" severity="success" @click="newItem"/>
+</template>
+
+<style scoped>
+
+</style>
