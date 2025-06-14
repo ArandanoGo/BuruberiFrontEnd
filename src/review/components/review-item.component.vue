a79fb21b7c6cec2b7912035b45f37ecee94ac088
AngeloC12 <angelocuri@hotmail.com>
2025-06-14 07:43:53 -0500

    feat: added stars  for  score

--- src/review/components/review-item.component.vue
+++ src/review/components/review-item.component.vue
@@ -1,16 +1,26 @@
 <script>
-
-
 import {Card  as PvCard} from "primevue";
 import {Review} from "../model/review.entity.js";
 
-export default{
+export default {
   name: "review-item",
-  components: {PvCard},
+  components: { PvCard },
   props: {
     review: {
       type: Review,
       required: true,
+    }
+  },
+  computed: {
+    estrellas() {
+      // Asumimos que review.puntuacion es un número entero entre 0 y 5
+      const maxEstrellas = 5;
+      const llenas = Math.min(Math.max(this.review.puntuacion, 0), maxEstrellas);
+      const vacias = maxEstrellas - llenas;
+      return {
+        llenas: Array(llenas).fill('★'),
+        vacias: Array(vacias).fill('☆')
+      };
     }
   }
 }
@@ -19,14 +29,17 @@ export default{
 <template>
   <pv-card class="review-card">
     <template #content>
-      {{review.comentario}}
+      {{ review.comentario }}
     </template>
     <template #footer>
-      <p>{{ review.puntuacion}}</p>
+      <p>
+        <span v-for="(estrella, index) in estrellas.llenas" :key="'llena-' + index" style="color: gold; font-size: 1.2em;">
+          {{ estrella }}
+        </span>
+        <span v-for="(estrella, index) in estrellas.vacias" :key="'vacia-' + index" style="color: lightgray; font-size: 1.2em;">
+          {{ estrella }}
+        </span>
+      </p>
     </template>
   </pv-card>
 </template>
-
-<style scoped>
-
-</style>
