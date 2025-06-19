c621c35574400e18028d3e9b9fe4e8f964723d44
AngeloC12 <angelocuri@hotmail.com>
2025-06-19 02:02:00 -0500

    fafaf

--- src/Chat/model/mensaje.entity.js
+++ src/Chat/model/mensaje.entity.js
@@ -5,13 +5,11 @@ export class MensajeEntity {
                     destinatarioId = null,
                     contenido = "",
                     fechaEnvio = new Date(),
-                    leido = false,
                 } = {}) {
         this.id = id;
         this.remitenteId = remitenteId;       // ID del usuario que envía
         this.destinatarioId = destinatarioId; // ID del usuario que recibe
         this.contenido = contenido;
         this.fechaEnvio = new Date(fechaEnvio);
-        this.leido = leido;                   // booleano opcional para marcar si fue leído
     }
 }
